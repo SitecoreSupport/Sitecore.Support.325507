f764e9f293b27a07ced294a565274aa1955d9e81
SC-IDO <ido@sitecore.net>
2019-04-17 14:59:48 +0300

    Initial functionality

---
+++ src/Sitecore.Support.325507/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.325507")]
+[assembly: AssemblyProduct("Sitecore.Support.325507")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.325507/Shell/Framework/Pipelines/DeleteItems.cs
@@ -0,0 +1,88 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.Text;
+using Sitecore.Web.UI.Sheer;
+using System.Collections.Generic;
+
+namespace Sitecore.Support.Shell.Framework.Pipelines
+{
+  public class DeleteItems : Sitecore.Shell.Framework.Pipelines.DeleteItems
+  {
+    public override void Confirm([NotNull] ClientPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+
+      if (args.IsPostBack)
+      {
+        if (args.Result == "yes")
+        {
+          args.Result = string.Empty;
+        }
+        else if (args.Result == "no")
+        {
+          Database database = GetDatabase(args);
+          ListString list = new ListString(args.Parameters["items"], '|');
+
+          if (list.Count == 1)
+          {
+            var language = args.Parameters["language"];
+            Item item = GetItem(database, list, Language.Parse(language));
+
+            if (Context.Item == null || item.ID != Context.Item.ID)
+            {
+              Context.ClientPage.ClientResponse.Eval(string.Format("this.Content.loadSearchedItem('{0}')", item.ID));
+            }
+          }
+
+          args.AbortPipeline();
+        }
+      }
+      else
+      {
+        Database database = GetDatabase(args);
+        var items = GetItems(args);
+
+        Context.ClientPage.ClientResponse.Confirm(DeleteItems.GetConfirmDeleteItemsMessage(items, database, args.Parameters["language"]));
+
+        args.WaitForPostBack();
+      }
+    }
+
+    private static Database GetDatabase([NotNull] ClientPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      Database database = Factory.GetDatabase(args.Parameters["database"]);
+      Assert.IsNotNull(database, typeof(Database), "Name: {0}", args.Parameters["database"]);
+      return Assert.ResultNotNull(database);
+    }
+
+    private static List<Item> GetItems([NotNull] ClientPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      Database database = GetDatabase(args);
+      var result = new List<Item>();
+      var list = new ListString(args.Parameters["items"], '|');
+      foreach (string id in list)
+      {
+        Item item = database.GetItem(id, Language.Parse(args.Parameters["language"]));
+        if (item != null)
+        {
+          result.Add(item);
+        }
+      }
+      return Assert.ResultNotNull(result);
+    }
+
+    private static Item GetItem([NotNull] Database database, [NotNull] ListString list, Language language)
+    {
+      Assert.ArgumentNotNull(database, "database");
+      Assert.ArgumentNotNull(list, "list");
+      Item item = database.GetItem(list[0], language);
+      Assert.IsNotNull(item, typeof(Item), "ID: {0}", list[0]);
+      return Assert.ResultNotNull(item);
+    }
+  }
+}
